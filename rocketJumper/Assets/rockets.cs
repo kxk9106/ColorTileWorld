2617a27cef7b9962c11f5f005043b5350b92a90f
Justin John <jxj7701@IMM-20.main.ad.rit.edu>
2015-11-13 10:03:07 -0500

    began fire

---
+++ rocketJumper/Assets/rockets.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockets : MonoBehaviour {
+
+	public GameObject Projectile = null;
+	public Transform Launcher = null;
+	bool Ready = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			//shoot the shot
+		}
+
+
+		/*
+		if(Ready == true)
+		{
+			//Ready = false;
+			//FireProjectile();
+		}
+		else
+		{
+			//ReloadTimed += 0.1;
+			//Debug.Log(ReloadTimed);
+		}*/
+	}
+		
+	void FireProjectile()
+	{
+		GameObject.Instantiate(Projectile,Launcher.transform.forward,transform.rotation);
+	}
+
+	//maybe set up some kind of timer to stop people from spamming, something like 1s or .5s
+	void CheckReload()
+	{
+		if(ReloadTimed > ReloadTime)
+		{
+			ReloadTimed = 0;
+			Ready = true;
+		}
+	}
+	
+}
