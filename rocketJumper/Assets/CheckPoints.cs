6a024ecb99402e77665a94a25db1fe173a886f13
unknown <sp4216@NM2550-30.main.ad.rit.edu>
2015-11-15 16:42:18 -0500

    Check Points

---
+++ rocketJumper/Assets/CheckPoints.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	Vector3 restartLoc;
+
+	// Use this for initialization
+	void Start () {
+		restartLoc = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.y <= -1.5f) {
+			this.transform.position = restartLoc;
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		//if ball goes over tile -> changes color
+		if(other.gameObject.tag=="checkPoint"){
+			restartLoc = other.gameObject.transform.position;
+		}//if
+	}
+}
