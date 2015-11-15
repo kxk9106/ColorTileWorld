3d7f4029c09113478a6bae84d9ba810603f6f886
Kristen Kanekuni <kxk9106@NMM-29.main.ad.rit.edu>
2015-11-15 17:25:32 -0500

    working

--- rocketJumper/Assets/scripts/restartCheck.cs
+++ rocketJumper/Assets/scripts/restartCheck.cs
@@ -5,18 +5,23 @@ public class restartCheck : MonoBehaviour {
 	Vector3 restartLoc;
 	GameObject player;
 
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		restartLoc = player.transform.position;
+	}
+
 	
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
-		restartLoc = player.transform.position;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (player.transform.position.y <= -1.5f) {
-			player.transform.position = restartLoc;
-			Debug.Log("triggered");
+			player.transform.position = new Vector3(restartLoc.x,restartLoc.y,restartLoc.z);
+			Debug.Log ("position: " + restartLoc);
+
 		}
 	}
 
