968e2b4a3959b175576af25a2084a71d13c23776
Kristen Kanekuni <kxk9106@NMM-29.main.ad.rit.edu>
2015-11-15 17:18:22 -0500

    trigger

---
+++ rocketJumper/Assets/scripts/restartCheck.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class restartCheck : MonoBehaviour {
+	Vector3 restartLoc;
+	GameObject player;
+
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		restartLoc = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.y <= -1.5f) {
+			player.transform.position = restartLoc;
+			Debug.Log("triggered");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "player") {
+			player.transform.position = restartLoc;
+			Debug.Log ("hi");
+		}
+
+	}
+}
